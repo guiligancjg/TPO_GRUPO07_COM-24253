bc0d95a34f2cfd94ee361ccce46b1bc72b86c1e1
Jorge Gramajo <Jorge Gramajo>
2024-04-29 16:23:47 -0300

    Se agrego event

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 function openNav(){
+    event.preventDefault();
     document.getElementById("mobile-menu").style.width = "100%";
     document.querySelector(".close").style.right = "5%";
 }
 
 
 function closeNav(){
+    event.preventDefault();
     document.getElementById("mobile-menu").style.width = "0%";
     document.querySelector(".close").style.right = "100%";
 }
