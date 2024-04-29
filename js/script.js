972c91368af04824abf7f4a0e5096740c3c5364d
Jorge Gramajo <Jorge Gramajo>
2024-04-29 15:55:00 -0300

    Se agrego el menu de navegación a todas las paginas

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
 function openNav(){
     document.getElementById("mobile-menu").style.width = "100%";
+    document.querySelector(".close").style.right = "5%";
 }
+
 
 function closeNav(){
     document.getElementById("mobile-menu").style.width = "0%";
+    document.querySelector(".close").style.right = "100%";
 }
+
+
+const div = document.querySelector(".header__logo");
+
+div.addEventListener("click", function() {
+    window.location.href = "index.html";
+});
